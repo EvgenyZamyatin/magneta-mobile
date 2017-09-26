d0834d104623a9800e32cc54dd396d310fc46596
evgenyzamyatin <jeka_zamyatin@mail.ru>
2017-09-27 00:09:08 +0300

    init

---
+++ scripts/optimize.sh
@@ -0,0 +1,11 @@
+python3 -m tensorflow.python.tools.optimize_for_inference \
+  --input=$1 \
+  --output="$2" \
+  --input_names="input_x,input_alpha" \
+  --output_names="output"
+
+python3 -m scripts.quantize_graph \
+  --input="$2" \
+  --output="$2" \
+  --output_node_names="output" \
+  --mode=weights_rounded
