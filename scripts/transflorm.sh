ef12e5bc113524a25f276529374202fab33734cb
evgenyzamyatin <jeka_zamyatin@mail.ru>
2017-09-28 21:41:25 +0300

    johnson 9->3, mob, bn-in, same-pad, -2 resid, reduce-dims, no-resid

--- scripts/transflorm.sh
+++ scripts/transflorm.sh
@@ -2,4 +2,4 @@
 --in_graph=$1 --out_graph=$2 \
 --inputs='input_x' \
 --outputs='output' \
---transforms='add_default_attributes strip_unused_nodes(type=float, shape="1,256,256,3") remove_nodes(op=CheckNumerics) fold_constants(ignore_errors=true) fold_batch_norms fold_old_batch_norms quantize_weights quantize_nodes strip_unused_nodes sort_by_execution_order'
+--transforms='add_default_attributes strip_unused_nodes(type=float, shape="1,256,256,3") remove_nodes(op=CheckNumerics) fold_constants(ignore_errors=true) fold_batch_norms fold_old_batch_norms quantize_weights strip_unused_nodes sort_by_execution_order'
